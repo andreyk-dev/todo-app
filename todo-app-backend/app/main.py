2c7a30a78b7d6cbb4a159d9abaf39e30a046f948
Andrey <k4pleev@yandex.ru>
2026-09-15 15:49:07 +0300

    current change main.py

--- todo-app-backend/app/main.py
+++ todo-app-backend/app/main.py
@@ -6,15 +6,6 @@ from app.api.routers.task import router as task_router
 from app.api.routers.category import router as category_router
 
 
-<<<<<<< HEAD
-=======
-# @asynccontextmanager
-# async def lifespan(_: FastAPI):
-#     # Base.metadata.create_all(bind=engine)
-#     yield
-
-
->>>>>>> 2dcb89290b22b47b6d61acee155fb862892c948b
 app = FastAPI()
 app.include_router(router=task_router)
 app.include_router(router=category_router)
